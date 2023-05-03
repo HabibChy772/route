b31a847eb0a669505896325a3ec56b4141a5ccd6
HabibChy772 <habibchy772@gmail.com>
2023-05-03 19:24:51 +0600

    first commit

---
+++ src/Bio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Bio = () => {
+  return (
+    <div>Bio
+        
+         <Link to={'/Date'}>date</Link>
+    </div>
+  )
+}
+
+export default Bio
---
+++ src/Mm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const 
+mm = () => {
+  return (
+    <div>
+      mmcacx
+
+      <Outlet/>
+    </div>
+  )
+}
+
+export default 
+mm
